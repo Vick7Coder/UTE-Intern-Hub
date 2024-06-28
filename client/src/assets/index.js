3bf7ec3757952fcc60473dff69c5aa8b1da17ff6
hieuph <hieuph.dev@gmail.com>
2024-06-28 17:24:24 +0700

    update banner

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -1,14 +1,13 @@
 import NoProfile from "./userprofile.png";
-import HeroImage from "./hero.png";
 import Office from "./office.jpg";
-import JobImg from "./job.jpg";
+import JobImg from "./jsls.gif";
 import TreegrowImg from "./treeGrow.jpg";
-
+import F88 from "./utebanner.gif";
 
 export {
   NoProfile,
-  HeroImage,
   Office,
   JobImg,
   TreegrowImg,
+  F88,
 };
